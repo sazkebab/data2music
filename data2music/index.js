70ba7fa57300bd38fe9807141176362ec7224ff3
Sarah Crooke <sarahcrooke@Sarahs-MBP.lan>
2017-07-11 21:19:25 +1000

    update

--- data2music/index.js
+++ data2music/index.js
@@ -1,19 +1,35 @@
-var MidiWriter = require('midi-writer-js');
- 
-var track = new MidiWriter.Track();
- 
-track.addEvent([
-            new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-            new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-            new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-            new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-            new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
-            new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-            new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
-    ], function(event, index) {
-    return {sequential:true};
+var fs = require('fs');
+var parse = require('csv-parse');
+var async = require('async');
+var baudio = require('baudio');
+
+
+
+var inputFile='data/data.csv';
+var minTime = 0;
+var maxTime = 1440;
+var sessionsByCoutnry = {};
+
+
+var dataComplete = function (){
+    console.log(sessionsByCoutnry);
+    var n = 0;
+var b = baudio(function (t) {
+    var x = Math.sin(t * 262 + Math.sin(n));
+    n += Math.sin(t);
+    return x;
+});
+b.play();
+};
+var parser = parse({delimiter: ','}, function (err, data) {
+  for(var i in data){
+    if( sessionsByCoutnry[data[i][0]] === undefined){
+         sessionsByCoutnry[data[i][0]] = new Array();
+    }
+    sessionsByCoutnry[data[i][0]].push({"time":data[i][1],"sessions":data[i][2]});
   }
-);
- 
-var write = new MidiWriter.Writer([track]);
-console.log(write.dataUri());
+  dataComplete();
+});
+
+
+fs.createReadStream(inputFile).pipe(parser);
